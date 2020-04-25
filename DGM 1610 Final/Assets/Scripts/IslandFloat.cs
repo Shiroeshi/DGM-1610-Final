1b3119499ed08c14ceadba8cd2d1d65e8ba0ea30
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-04-24 20:00:34 -0600

    Final Project assembly. Had all the assets, just need to finish assembly

---
+++ DGM 1610 Final/Assets/Scripts/IslandFloat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandFloat : MonoBehaviour
+{
+    public float amplitude = 0.7f;
+    public float frequency = 1f;
+
+    // position storage for movement
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This will mark and record the starting position and rotation of the pickup for use in later variables
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This makes it float up and down using a Sin() algorithm
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
